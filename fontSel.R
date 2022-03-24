f02f0007926760b5f0a7309d40a65b0516b3d39e
dipperGit <shirakawa_hokuto26@fra.go.jp>
2022-03-24 15:43:38 +0900

    fontSel function #initial comment 20220324 15:43

---
+++ fontSel.R
@@ -0,0 +1,36 @@
+
+fontSel <- function(){
+  
+  #Rで日本語使えるようにするフォント指定。
+  #OS名を認識して、mac/winでそれそれフォントを指定する
+  #使えるフォントはnames(quartzFonts())で確認(macのみ)。フォントはPC依存。
+  
+  if(grepl("macOS", sessionInfo()$running) == TRUE){
+    #Macの場合
+    #fontregisterer関数がインストール済みか確認
+    exist <- "fontregisterer" %in% rownames(installed.packages())
+    
+    if(exist == TRUE){
+      #インストール済みの場合
+      require(fontregisterer)
+      fontA <<- "YuMincho" #ここでは遊明朝
+      cat("インストール済み、Mac最高～ \n")
+    }else{
+      #なかったらインストールして読み込み
+      install.packages("remotes")
+      install.packages("systemfonts")
+      remotes::install_github("Gedevan-Aleksizde/fontregisterer", repos = NULL, type = "source")
+      #https://ill-identified.hatenablog.com/entry/2020/10/03/200618
+      require(fontregisterer)
+      fontA <<- "YuMincho" #ここでは遊明朝
+      cat("インストール完了、Mac最高～ \n")
+    }
+    
+  }else{
+    #Windowsだった場合
+    windowsFonts(MEI = windowsFont("Meiryo")) #とりまメイリオ
+    fontA <<- "MEI" 
+    cat("windows11のGUIは8以来のマジギレ案件。Windowsユーザーは強く生きて。\n")
+  }
+  
+}#おわりんこ
